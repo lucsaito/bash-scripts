a1b926ec325c7a774b6a4ea0cea2183ef746f7bc
Lucas <lukinhassaito2013@hotmail.com>
2021-04-02 00:37:07 -0300

    scripts folder created

---
+++ scripts/hello-user.sh
@@ -0,0 +1,7 @@
+#!bin/bash
+# This is a basic script that reads the input "username"
+# and then greets the "username" with "Hello".
+
+echo "Hi! What is your name?"
+read username
+echo "Hello, ${username}!"
